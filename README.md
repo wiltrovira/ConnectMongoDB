d3b23c4d815879ff2a0dac147ab2e6b46b587167
Wilt Rovira <wrovira@gmail.com>
2021-12-19 09:53:14 -0500

    Conexión con mongoose

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ La cadena de conexión se debe guardar en un archivo .env (Este archivo no se su
 
 Nota: La base de datos puede estar en un servidor de MongoDB local, en Atlas MongoDB, Azure CosmosDb u otro servicio en la nube.
 
+
+## Ejemplos
+Se incorporan 2 ejemplos para conectar a la base de datos:
+1. Se conecta a través del driver de mongodb
+2. Se conecta a través de mongoose
 ## versiones
 node --version = v16.13.1
 npm -version = 8.3.0
@@ -17,3 +22,4 @@ npm -version = 8.3.0
 npm init -y
 npm install dotenv  (versión 10.0.0)
 npm install mongodb (versión 4.2.2)
+npm install mongoose (versión 6.1.2)
---
+++ dbconnectmongoose.js
@@ -0,0 +1,34 @@
+/**
+ * Para ejecutar este módulo, crear un archivo con el nombre ".env"
+ * Agregar una línea con la variable
+ *          EXAMPLE_MONGODB_CONNSTRING=cadenaConexionMongoDB
+ */
+require("dotenv").config(); //Requiere el módulo dotenv para cargar variables de entorno
+
+var mongoClient = require("mongoose");
+
+/**
+ * Abre la conexión a la base de datos on mongoClient
+ * La función conectarMongoDB() es un callback
+ */
+mongoClient.connect(
+    process.env.EXAMPLE_MONGODB_CONNSTRING,
+    callbackConectarMongoDB
+);
+
+/**
+ * Esta es una función callback que abre la cadena de conexión a la base de datos y procesa el error
+ * @param {*} err En caso de error al conectar, devuelve un objeto con el error
+ * @param {*} db  Objeto con la conexión a la base de datos
+ * @returns
+ */
+function callbackConectarMongoDB(err, db) {
+    if (err) {
+        console.log("Error al conectarse: ", err.message);
+        return;
+    }
+
+    console.log("Conexión a base de datos fue exitosa!!!");
+    db.close(); //Cierra la conexión a la base de datos
+    console.log("Conexión cerrada con éxito");
+}
